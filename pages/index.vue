24a3cf85281d50830afafee0549bea14069142a6
Abrar Dhalwala <amdhalwala@gmail.com>
2020-07-07 18:17:56 +0530

    Home page

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container mx-auto">
+    <home />
+  </div>
+</template>
+
+<script>
+import Home from '~/components/Home'
+
+export default {
+  components: {
+    Home,
+  },
+}
+</script>
+
+<style></style>
