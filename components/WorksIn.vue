0df0b010bfefdfa75036614134d2b64fba738d14
Abrar Dhalwala <amdhalwala@gmail.com>
2020-07-15 11:42:10 +0530

    Works Animation fixes

--- components/WorksIn.vue
+++ components/WorksIn.vue
@@ -8,18 +8,19 @@
           :key="language.name"
           class="w-full md:w-1/3 text-lg md:text-3xl font-semibold mb-5 md:mb-10 flex justify-center"
           :class="language.color"
-          data-aos="fade-up"
-          data-aos-offset="10"
-          :data-aos-delay="delayValue(index)"
         >
           <div
-            class="h-12 w-12 md:h-20 md:w-20 work-item flex items-center rounded-lg bg-gray-800 p-2 cursor-default transition-all duration-500 ease-in-out transform hover:shadow-xl hover:m-0 hover:w-3/4 hover:justify-center"
+            class="h-12 w-12 md:h-20 md:w-20 work-item flex items-center rounded-lg bg-gray-800 p-2 cursor-default transform hover:shadow-xl hover:m-0 hover:w-3/4 hover:justify-center"
+            :class="language.hoverColor"
+            data-aos="fade-up"
+            data-aos-offset="100"
+            :data-aos-delay="delayValue1(index)"
           >
             <div
               class="ml-4 md:ml-8 work-item-inner"
               data-aos="fade-left"
-              data-aos-offset="10"
-              :data-aos-delay="delayValue(index)"
+              data-aos-offset="100"
+              :data-aos-delay="delayValue2(index)"
             >
               {{ language.name }}
             </div>
@@ -42,36 +43,52 @@ export default {
       languages: [
         {
           name: 'PHP',
-          color: 'text-teal-500'
+          color: 'text-teal-500',
+          hoverColor: 'hover:bg-teal-500 hover:text-gray-800'
         },
         {
           name: 'Laravel',
-          color: 'text-red-500'
+          color: 'text-red-500',
+          hoverColor: 'hover:bg-red-500 hover:text-gray-800'
         },
         {
           name: 'Javascript',
-          color: 'text-yellow-500'
+          color: 'text-yellow-500',
+          hoverColor: 'hover:bg-yellow-500 hover:text-gray-800'
         },
         {
           name: 'Angular',
-          color: 'text-red-500'
+          color: 'text-red-500',
+          hoverColor: 'hover:bg-red-500 hover:text-gray-800'
         },
         {
           name: 'Vue',
-          color: 'text-green-500'
+          color: 'text-green-500',
+          hoverColor: 'hover:bg-green-500 hover:text-gray-800'
         }
       ]
     }
   },
   methods: {
-    delayValue(a) {
+    delayValue1(a) {
       return 100 + 100 * a
+    },
+    delayValue2(a) {
+      return 500 + 100 * a
     }
   }
 }
 </script>
 
 <style scoped>
+/* Tailwind and AOS library property conflict resolve */
+.work-item {
+  transition: transform 1s ease 0.3s, opacity 1s ease 0.3s,
+    background-color 0.5s ease-in-out, color 0.5s ease-in-out,
+    box-shadow 0.5s ease-in-out, width 0.5s ease-in-out,
+    margin 0.5s ease-in-out 0.5s !important;
+}
+
 .work-item .work-item-inner {
   pointer-events: none;
   transition: opacity 1s, transform 1s, margin 1.5s !important;
